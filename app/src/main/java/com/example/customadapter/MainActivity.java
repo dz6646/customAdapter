5b08799c9814cf7446b8c3fcfa3db6b6d2c4abdc
Dan Zhuravlov <yuoirty@gmail.com>
2022-11-29 10:51:02 +0200

    yes fun

--- app/src/main/java/com/example/customadapter/MainActivity.java
+++ app/src/main/java/com/example/customadapter/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.customadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] names = {"Choose a country:", "Israel", "USA", "Brazil", "Russia", "Isle of man",
+    "Spain", "Afghanistan"};
+
+    int[] imgid = {R.drawable.israel, R.drawable.usa, R.drawable.brazil, R.drawable.russia,
+    R.drawable.isleman, R.drawable.spain, R.drawable.afgan};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomAdapter customadp = new CustomAdapter(getApplicationContext(),
+                names, imgid);
+
+
     }
 }
